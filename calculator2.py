e941d1f4565f9be0e65c6cdd1f6d2180df5db4c0
OzsvathHunor <ozshunor@gmail.com>
2018-12-11 20:05:57 +0100

    if hozzaadva

--- calculator2.py
+++ calculator2.py
@@ -8,12 +8,16 @@ operation = raw_input("Choose math operation (+, -, *, /: ")
 print operation
 
 if operation == "+":
+    print "osszeadas"
     print x + y
 elif operation == "-":
+    print "kivonas"
     print x - y
 elif operation == "*":
+    print "szorzas"
     print x * y
 elif operation == "/":
+    print "osztas"
     print x / y
 else:
     print "You did not provide the correct math operation."
